4c88936c6f8788ef7face83c2a7d4bf2c52aa8f7
Kirill Morozov <grifx.design@gmail.com>
2024-04-25 18:02:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from gevent import monkey
+monkey.patch_all()
+
+from flask import Flask, request
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Используйте gevent в качестве асинхронного режима для SocketIO
+socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
+CORS(app)  # Включение CORS для всех источников
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/flows/trigger/d3a740f4-8aef-4688-a591-d8bed4373cb7', methods=['POST'])
+def update_room():
+    data = request.json
+    # Отправка события всем подключённым клиентам
+    socketio.emit('update', data)
+    return {"success": True}, 200
+
+if __name__ == '__main__':
+    # Запуск Flask-приложения с использованием gevent
+    socketio.run(app, debug=False, host='0.0.0.0', port=8081)
